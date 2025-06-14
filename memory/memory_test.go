5d989e0abe6463fee3320a19550f6949ef92dc03
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2025-06-14 15:42:30 +0530

    Test: Add test scenarios for pub/sub

--- memory/memory_test.go
+++ memory/memory_test.go
@@ -1,15 +1,233 @@
 package memory_test
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/Suhaan-Bhandary/pubsub"
+	"github.com/Suhaan-Bhandary/pubsub/memory"
 )
 
 // TestEvent contains data of type string and name of type whose underlying type is string
-type TestEvent pubsub.EventEnvelope[string, string]
+type TestEvent pubsub.EventEnvelope[string, int]
 
 // TestBasicExample tests the basic scenario with one publisher and one subscriber
 func TestBasicExample(t *testing.T) {
-	t.Fatal("unimplemented")
+	publisher := memory.NewPublisher[TestEvent]()
+	defer publisher.Close()
+
+	subscriber := memory.NewSubscriber[TestEvent]()
+
+	publisher.Subscribe(subscriber)
+
+	// Go routine to publish events
+	go func() {
+		for i := range 3 {
+			publisher.Publish(TestEvent{
+				Name: "TEST",
+				Data: i,
+			})
+		}
+	}()
+
+	expected := 0
+	for {
+		event, ok := subscriber.Listen()
+		if !ok {
+			break
+		}
+
+		if expected != event.Data {
+			t.Errorf("error listening to socket, expected: %d, got: %d", expected, event.Data)
+		}
+		expected++
+	}
+
+	publisher.UnSubscribe(subscriber)
+}
+
+// TestMultipleSubscriber check working of one publisher with multiple subscribers
+func TestMultipleSubscriber(t *testing.T) {
+	publisher := memory.NewPublisher[TestEvent]()
+	defer publisher.Close()
+
+	subscriber1 := memory.NewSubscriber[TestEvent]()
+	subscriber2 := memory.NewSubscriber[TestEvent]()
+
+	publisher.Subscribe(subscriber1)
+	publisher.Subscribe(subscriber2)
+
+	// Go routine to publish events
+	go func() {
+		for i := range 3 {
+			publisher.Publish(TestEvent{
+				Name: "TEST",
+				Data: i,
+			})
+		}
+	}()
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+
+		expected := 0
+		for {
+			event, ok := subscriber1.Listen()
+			if !ok {
+				break
+			}
+
+			if expected != event.Data {
+				t.Errorf("error listening to socket, expected: %d, got: %d", expected, event.Data)
+			}
+			expected++
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		expected := 0
+		for {
+			event, ok := subscriber2.Listen()
+			if !ok {
+				break
+			}
+
+			if expected != event.Data {
+				t.Errorf("error listening to socket, expected: %d, got: %d", expected, event.Data)
+			}
+			expected++
+		}
+	}()
+
+	wg.Wait()
+}
+
+// TestMultiplePublisher check working of multiple publisher with single subscriber
+func TestMultiplePublisher(t *testing.T) {
+	publisher1 := memory.NewPublisher[TestEvent]()
+	defer publisher1.Close()
+
+	publisher2 := memory.NewPublisher[TestEvent]()
+	defer publisher2.Close()
+
+	subscriber := memory.NewSubscriber[TestEvent]()
+	publisher1.Subscribe(subscriber)
+	publisher2.Subscribe(subscriber)
+
+	// Go routine to publish events
+	go func() {
+		for i := range 3 {
+			publisher1.Publish(TestEvent{
+				Name: "TEST-1",
+				Data: i,
+			})
+		}
+	}()
+
+	go func() {
+		for i := range 3 {
+			publisher2.Publish(TestEvent{
+				Name: "TEST-2",
+				Data: i,
+			})
+		}
+	}()
+
+	readCount := 0
+	for {
+		_, ok := subscriber.Listen()
+		if !ok {
+			break
+		}
+
+		readCount++
+	}
+
+	expectedReadCount := 6
+	if readCount != expectedReadCount {
+		t.Errorf("error listening to socket, expected: %d, got: %d", expectedReadCount, readCount)
+	}
+}
+
+// TestMultiplePublishserSubscriber check working of multiple publisher with multiple subscribers
+func TestMultiplePublishserSubscriber(t *testing.T) {
+	publisher1 := memory.NewPublisher[TestEvent]()
+	defer publisher1.Close()
+
+	publisher2 := memory.NewPublisher[TestEvent]()
+	defer publisher2.Close()
+
+	subscriber1 := memory.NewSubscriber[TestEvent]()
+	subscriber2 := memory.NewSubscriber[TestEvent]()
+
+	publisher1.Subscribe(subscriber1)
+	publisher1.Subscribe(subscriber2)
+
+	publisher2.Subscribe(subscriber1)
+	publisher2.Subscribe(subscriber2)
+
+	// Go routine to publish events
+	go func() {
+		for i := range 3 {
+			publisher1.Publish(TestEvent{
+				Name: "TEST-1",
+				Data: i,
+			})
+		}
+	}()
+
+	go func() {
+		for i := range 3 {
+			publisher2.Publish(TestEvent{
+				Name: "TEST-2",
+				Data: i,
+			})
+		}
+	}()
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+
+		readCount := 0
+		for {
+			_, ok := subscriber1.Listen()
+			if !ok {
+				break
+			}
+			readCount++
+		}
+
+		expectedReadCount := 6
+		if readCount != expectedReadCount {
+			t.Errorf("error listening to socket, expected: %d, got: %d", expectedReadCount, readCount)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		readCount := 0
+		for {
+			_, ok := subscriber2.Listen()
+			if !ok {
+				break
+			}
+			readCount++
+		}
+
+		expectedReadCount := 6
+		if readCount != expectedReadCount {
+			t.Errorf("error listening to socket, expected: %d, got: %d", expectedReadCount, readCount)
+		}
+	}()
+
+	wg.Wait()
 }
